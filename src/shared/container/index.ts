60f334e6e100e7de5f7f1cbc9d1d6ce89a8baa70
lucas.coelho <ludico1959@gmail.com>
2023-10-30 02:44:31 -0300

    business: register account repository singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { container } from 'tsyringe';
 import { IPeopleRepository } from '../../modules/people/repositories/IPeopleRepository';
 import { PeopleRepository } from '../../modules/people/repositories/implementations/PeopleRepository';
+import { IAccountsRepository } from '../../modules/accounts/repositories/IAccountsRepository';
+import { AccountsRepository } from '../../modules/accounts/repositories/implementations/AccountsRepository';
 
 container.registerSingleton<IPeopleRepository>(
   'PeopleRepository',
   PeopleRepository,
+);
+
+container.registerSingleton<IAccountsRepository>(
+  'AccountsRepository',
+  AccountsRepository,
 );
