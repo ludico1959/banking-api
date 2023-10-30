1c8ef4b93c4f7fa88caee240e4f1eec2e4f45735
lucas.coelho <ludico1959@gmail.com>
2023-10-30 03:58:49 -0300

    fix: correct regex validator for branch, cvv and account numbers

--- src/utils/validatorRegex.ts
+++ src/utils/validatorRegex.ts
@@ -1,11 +1,11 @@
 export function validateAccount(account: string): boolean {
-  const regex = /\d{7}-\d{1}/i;
+  const regex = /\d{7}-\d{1}$/i;
 
   return regex.test(account);
 }
 
 export function validateBranchAndCvv(digits: string): boolean {
-  const regex = /\d{3}/i;
+  const regex = /\d{3}$/i;
 
   return regex.test(digits);
 }
