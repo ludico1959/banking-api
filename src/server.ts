abb89849bebe4d09d5f1c3b177fcda72cefe3fdd
lucas.coelho <ludico1959@gmail.com>
2023-10-30 12:48:06 -0300

    build: delete swagger related npm dependencies

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
 import './shared/container';
 import { router } from './routes';
-import swaggerFile from './swagger.json';
 import { AppError } from './errors/AppError';
 
 const app = express();
 
 app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.use(
