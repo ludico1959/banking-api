da6dca13f97750bbfd87037ccf4d0e32348fa82c
lucas.coelho <ludico1959@gmail.com>
2023-11-01 18:41:23 -0300

    build: set server port through env variable

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,6 +1,7 @@
+import 'dotenv/config';
 import app from './app';
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
